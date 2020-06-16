2da74e77f68a71e2a24ffc26d9e1d70280f98fb6
Ivana Begovic <ivana.begovic@crypterium.com>
2020-06-16 10:28:16 +0300

    added themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from "@angular/fire/firestore";
-import {Observable} from "rxjs";
-import {ApiService} from "../api.service";
-import {WeatherPopupComponent} from "./weather-popup/weather-popup.component";
-import {MatDialog} from "@angular/material/dialog";
-import {TaskInterface} from "./models/task.interface";
-import {WeeklyForecastInterface} from "./models/weekly-forecast.interface";
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {Observable} from 'rxjs';
+import {ApiService} from '../api.service';
+import {WeatherPopupComponent} from './weather-popup/weather-popup.component';
+import {MatDialog} from '@angular/material/dialog';
+import {TaskInterface} from './models/task.interface';
+import {WeeklyForecastInterface} from './models/weekly-forecast.interface';
+import {OverlayContainer} from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -14,24 +15,36 @@ import {WeeklyForecastInterface} from "./models/weekly-forecast.interface";
 })
 export class AppComponent implements OnInit {
   public tasks: Observable<any[]>;
-  public length: number = 0;
-  public isTaskFormShown: boolean = false;
+  public length = 0;
+  public isTaskFormShown = false;
   public editItem: any = null;
+  @HostBinding('class') componentCssClass;
   private itemsCollection: AngularFirestoreCollection<TaskInterface>;
   private $weeklyForecast: Observable<WeeklyForecastInterface[]> = this.serverApi.getWeeklyForecast();
+  private themeClicked: number = 0;
+  private themeClasses: string[] = [
+    'purple-theme', 'yellow-theme'
+  ];
 
-  constructor(private tasksBd: AngularFirestore, private serverApi: ApiService, public dialog: MatDialog) {
+  constructor(private tasksBd: AngularFirestore, private serverApi: ApiService,
+              public dialog: MatDialog,
+              public overlayContainer: OverlayContainer) {
     this.itemsCollection = tasksBd.collection<TaskInterface>('tasks');
-    this.itemsCollection.valueChanges().subscribe((a) => {
-      console.log(a);
-      this.length = a.length;
-    })
+    this.itemsCollection.valueChanges().subscribe((a) => this.length = a.length);
     this.tasks = this.itemsCollection.valueChanges({idField: 'taskId'});
+  }
+
+  public onThemeChange() {
+    this.themeClicked = this.themeClicked + 1;
+    this.themeClicked = this.themeClicked % this.themeClasses.length;
+    const theme = this.themeClasses[this.themeClicked];
+    this.overlayContainer.getContainerElement().classList.add(theme);
+    this.componentCssClass = theme;
   }
 
   public ngOnInit() {
     this.serverApi.getAdminToken().subscribe(() => {
-    })
+    });
     //
     // this.serverApi.getWeeklyForecast().subscribe((a) => {
     //   this.weeklyForecast
@@ -40,8 +53,8 @@ export class AppComponent implements OnInit {
   }
 
   public onAdditionClick() {
-    this.isTaskFormShown = true
-    this.editItem = null
+    this.isTaskFormShown = true;
+    this.editItem = null;
   }
 
   public onSettingsButtonClick() {
@@ -54,19 +67,19 @@ export class AppComponent implements OnInit {
 
   public addElement(item: TaskInterface) {
     if (this.editItem) {
-      this.tasksBd.doc<TaskInterface>('tasks/' + item.taskId).update(item)
+      this.tasksBd.doc<TaskInterface>('tasks/' + item.taskId).update(item);
     } else {
-      this.itemsCollection.add(item)
+      this.itemsCollection.add(item);
     }
-    this.isTaskFormShown = false
+    this.isTaskFormShown = false;
   }
 
   public onDeleteClick(id: number) {
-    this.tasksBd.doc<TaskInterface>('tasks/' + id).delete()
+    this.tasksBd.doc<TaskInterface>('tasks/' + id).delete();
   }
 
   public onEditClick(task) {
-    this.editItem = {...task}
-    this.isTaskFormShown = true
+    this.editItem = {...task};
+    this.isTaskFormShown = true;
   }
 }
--- src/app/item-form/item-form.component.ts
+++ src/app/item-form/item-form.component.ts
@@ -1,6 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
-import {TaskInterface} from "../models/task.interface";
+import {TaskInterface} from '../models/task.interface';
 
 @Component({
   selector: 'app-item-form',
@@ -19,20 +19,20 @@ export class ItemFormComponent implements OnInit {
     text: '',
     date: new Date(),
     taskId: ''
-  }
+  };
 
-  constructor(private formBuilder: FormBuilder,) {
+  constructor(private formBuilder: FormBuilder, ) {
     this.taskForm = this.formBuilder.group(this.initialState);
   }
 
   public ngOnInit() {
     if (this.item && typeof this.item !== 'undefined') {
-      this.taskForm.setValue(this.item)
+      this.taskForm.setValue(this.item);
     }
   }
 
   public onCancelClick() {
-    this.taskForm.reset()
-    this.canceled.emit()
+    this.taskForm.reset();
+    this.canceled.emit();
   }
 }
